8b13b05c7118e6aab23988b729d39073a7bf454d
Jeff Khuu <heyjeffkhuu@gmail.com>
2025-07-13 10:37:36 -0600

    New test auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/SpecimenAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/SpecimenAuto.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes.auto;
 import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -31,16 +32,18 @@ public class SpecimenAuto extends LinearOpMode {
                 robot.driveTrain.getDrive().actionBuilder(initialPose)
                         .splineToConstantHeading(new Vector2d(5, -32), 90)
                         .stopAndAdd(robot.moveToScore()) // Score preloaded specimen
+                        .waitSeconds(0.2)
+                        .stopAndAdd(robot.scoreSpecimen())
                         .strafeTo(new Vector2d(5, -40))
                         .stopAndAdd(robot.collectFromWall())
 
                         .splineToConstantHeading(new Vector2d(48, -40), 90)
                         .stopAndAdd(robot.extendCollection()) // Grab first specimen
-                        .stopAndAdd(robot.passthrough())
-                        .splineToConstantHeading(new Vector2d(48, -60), 90)
-                        .stopAndAdd(robot.moveToScore()) // Deposit and grab from observation zone
-                        .splineToConstantHeading(new Vector2d(7, -32), 90)
-                        .stopAndAdd(robot.collectFromWall()) // Score first specimen
+//                        .stopAndAdd(robot.passthrough())
+////                        .splineToConstantHeading(new Vector2d(48, -60), 90)
+////                        .stopAndAdd(robot.moveToScore()) // Deposit and grab from observation zone
+////                        .splineToConstantHeading(new Vector2d(7, -32), 90)
+////                        .stopAndAdd(robot.collectFromWall()) // Score first specimen
                         .build()
         );
     }
